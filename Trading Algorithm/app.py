28038ea4b529f7147ac6469ba0036e287fe98adb
Victor Jotham Ashioya <victorashioya960@gmail.com>
2023-06-16 23:53:12 +0300

    Update app.py

--- Trading Algorithm/app.py
+++ Trading Algorithm/app.py
@@ -1,2 +1,3 @@
 ## Objectives
+  # incorporate sentiment analysis into automated trading systems
   # to evaluate whether a given headline was indicative of Good News, Bad News, or Unknown, also asking for a brief explanation
