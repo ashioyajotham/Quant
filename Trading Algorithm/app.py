2aec48c35ce7a3fa33796619cfef5073c3d8bfa4
Victor Jotham Ashioya <victorashioya960@gmail.com>
2023-06-17 01:03:10 +0300

    Update app.py

--- Trading Algorithm/app.py
+++ Trading Algorithm/app.py
@@ -1,3 +1,35 @@
 ## Objectives
   # incorporate sentiment analysis into automated trading systems
   # to evaluate whether a given headline was indicative of Good News, Bad News, or Unknown, also asking for a brief explanation
+  
+  """
+  There Are Two Ways to Build Algorithmic Trading Systems.
+(Pro tip: If you don't want to wait until tomorrow for the next lesson, click the link at the bottom to get the next lesson in your inbox right away!)
+
+# i) The first is data mining.
+
+A trading system based on data mining looks for patterns in past price data and fits a model to them.
+
+The only assumption is that the patterns of the past will repeat in the future. This is where most people start throwing in machine learning.
+
+***********************Few successful trading systems are built through data mining***************************
+
+
+# ii) The second is modeling market inefficiencies.
+
+Model-based systems start with a model of a market inefficiency.
+
+Inefficiencies can be based on trader psychology, economics, market microstructure, company events, or anything else that affects the price.
+
+These inefficiencies cause patterns that deviate from the normal randomness of the market.
+
+Sometimes, these patterns repeat and can be detected, predicted, and traded.
+
+*******************************Most successful algorithmic trading systems are built by modeling market inefficiencies*************************
+
+An edge is a market anomaly that consistently, and non-randomly, makes you money.
+
+Algorithmic trading is a constant cycle of hypothesis formation and testing. This is why you learned Minimum Viable Python. 
+You need to cycle through ideas as fast as you can since most of them will not work.
+"""
+                                                                                          
